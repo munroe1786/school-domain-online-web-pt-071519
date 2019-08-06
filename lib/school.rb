96a3091d692f7d1cb7c94ec42182cbc5af715888
Eliza Munroe <era3munroe@gmail.com>
2019-08-06 18:52:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  attr_reader :school, :roster
+  
+  def initialize(school, roster)
+    @school = school
+    @roster = roster
+  end
+  
+  roster = {}
+  
+  def add_student(name, grade)
+    
+  end
+end
